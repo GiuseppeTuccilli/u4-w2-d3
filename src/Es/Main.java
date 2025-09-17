5da883b0f69abeebdd220647de61b2d12db95121
GiuseppeTuccilli <giuseppetuccilli@gmail.com>
2025-09-17 15:52:45 +0200

    com5

--- src/Es/Main.java
+++ src/Es/Main.java
@@ -23,7 +23,9 @@ public class Main {
         Customer cliente1 = new Customer("pippo", 2);
         Customer cliente2 = new Customer("pluto", 2);
         Customer cliente3 = new Customer("tizio", 2);
+
         ArrayList<Product> listaProd = new ArrayList<>();
+
         listaProd.add(prodotto1);
         listaProd.add(prodotto2);
         listaProd.add(prodotto3);
@@ -34,6 +36,7 @@ public class Main {
         Order ordine1 = new Order(cliente1, listaProd);
 
         ArrayList<Product> listaProd2 = new ArrayList<>();
+
         listaProd2.add(prodotto5);
         listaProd2.add(prodotto6);
         listaProd2.add(prodotto7);
@@ -42,6 +45,7 @@ public class Main {
         Order ordine2 = new Order(cliente2, listaProd2);
 
         ArrayList<Product> listaProd3 = new ArrayList<>();
+
         listaProd3.add(prodotto1);
         listaProd3.add(prodotto2);
         listaProd3.add(prodotto3);
@@ -54,7 +58,9 @@ public class Main {
         Order ordine3 = new Order(cliente3, listaProd3);
 
         Predicate<Product> isBooks = product -> product.getCategory().equals("books");
+
         List<Product> books = listaProd.stream().filter(isBooks.and(product -> product.getPrice() > 100)).toList();
+
         System.out.println("lista prodotti books");
         System.out.println(books);
 
@@ -68,20 +74,19 @@ public class Main {
         LocalDate aprVentuno = LocalDate.of(2021, 4, 1);
         LocalDate marVentuno = LocalDate.of(2021, 3, 20);
 
-
         List<Order> babyOrd = orders.stream().filter(order ->
                 order.getProducts().stream().anyMatch(product -> product.getCategory().equals("baby"))
         ).toList();
+
         System.out.println("lista ordini con almeno un prodotto baby");
         System.out.println(babyOrd);
 
-
         List<Product> boysProds = listaProd3.stream().filter(product -> product.getCategory().equals("boys")).toList();
-
 
         for (int i = 0; i < babyOrd.size(); i++) {
             boysProds.get(i).discountPerc(10);
         }
+        
         System.out.println(boysProds);
 
         for (int i = 0; i < 2; i++) {
@@ -102,7 +107,6 @@ public class Main {
 
         System.out.println("Prodotti ordinati nel periodo di tempo");
         System.out.println(timeRangeProds);
-
 
     }
 }
